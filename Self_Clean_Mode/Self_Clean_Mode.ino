3734ccca272c6940b24af00ccff4c4fc372592b0
F1 race car team <ucichemecars@gmail.com>
2017-09-12 22:28:40 -0700

    Added comments to explain variables

--- Self_Clean_Mode/Self_Clean_Mode.ino
+++ Self_Clean_Mode/Self_Clean_Mode.ino
@@ -6,7 +6,7 @@ void setup() {
         TCCR1A = 0x23;//configure the TCCR1A register on mode of operation and TOP
         TCCR1B = 0x19; //configure the TCCR1B register on mode of operation and TOP
         OCR1A = 639; // limit by 16 bits, determine the frequency of the output
-        OCR1B = 0;
+        OCR1B = 0;//Ratio of OCR1B/OCR1A will determine duty cycle, 0 means off
         pinMode(CTR_PIN, OUTPUT); // enable the PWM output (you now have a PWM signal on digital pin 10)
         Serial.begin(9600);
 
@@ -15,7 +15,7 @@ void setup() {
 void loop() {
         static long int start_time=millis();
         if((millis()-start_time)<CLEAN_TIME) {
-                OCR1B = 639; // the ratio of OCR1B/OCR1A will set duty cycle
+                OCR1B = 639; // 100% duty cycle
         }
         else{
                 OCR1B = 0;
