48b08cc30ec686c88a42a433c1dc9e6925d5625b
F1 race car team <ucichemecars@gmail.com>
2017-09-12 22:09:56 -0700

    Decrease Cleaning time and added missing variables
    Work well and relatively accurate timing.

--- Self_Clean_Mode/Self_Clean_Mode.ino
+++ Self_Clean_Mode/Self_Clean_Mode.ino
@@ -1,25 +1,26 @@
 //used for cleaning the pump by pumping DI water through the system
-#define CLEAN_TIME 20000//unit is ms
+#define CLEAN_TIME 12000//unit is ms
+#define CTR_PIN 10
 
 void setup() {
-        CCR1A = 0x23;//configure the TCCR1A register on mode of operation and TOP
+        TCCR1A = 0x23;//configure the TCCR1A register on mode of operation and TOP
         TCCR1B = 0x19; //configure the TCCR1B register on mode of operation and TOP
         OCR1A = 639; // limit by 16 bits, determine the frequency of the output
-
+        OCR1B = 0;
         pinMode(CTR_PIN, OUTPUT); // enable the PWM output (you now have a PWM signal on digital pin 10)
         Serial.begin(9600);
 
 }
 
 void loop() {
-        static long int start_time;
+        static long int start_time=millis();
         if((millis()-start_time)<CLEAN_TIME) {
                 OCR1B = 639; // the ratio of OCR1B/OCR1A will set duty cycle
         }
         else{
-                digitalWrite(CTR_PIN, LOW);
+                OCR1B = 0;
                 Serial.println("Done Cleaning");
-                delay(1000);
+
         }
 
 
