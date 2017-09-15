1d083c1847b10fe281f08fb48ea26bf048ca21b1
F1 race car team <ucichemecars@gmail.com>
2017-09-15 12:26:40 -0700

    Test with different amounts

--- Chemical_Measurement_Machine/Chemical_Measurement_Machine.ino
+++ Chemical_Measurement_Machine/Chemical_Measurement_Machine.ino
@@ -6,7 +6,7 @@
  #define REDUCED_PUMP_RATE 50 //unit is mL/s
  #define Pump_Time(pump_amount,SLOW_VOLUME) ((1.0-SLOW_VOLUME/100.0)*(pump_amount)/(FULL_PUMP_RATE) + (SLOW_VOLUME/100.0)*pump_amount/(REDUCED_PUMP_RATE))
  */
-float test_amount=9.0;//unit is mL
+float test_amount=25.0;//unit is mL
 
 void setup() {
         TCCR1A = 0x23;//configure the TCCR1A register on mode of operation and TOP
