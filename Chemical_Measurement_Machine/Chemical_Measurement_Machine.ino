68d5d3f2f5c2b7a451f1b58ac55a9383f5503239
F1 race car team <ucichemecars@gmail.com>
2017-09-20 00:17:11 -0700

    Rewrote the code to add support for multiple pumps, still need field tests
    Added class for pump

--- Chemical_Measurement_Machine/Chemical_Measurement_Machine.ino
+++ Chemical_Measurement_Machine/Chemical_Measurement_Machine.ino
@@ -1,34 +1,56 @@
+#define BIT(x) (0x01<<(x))//create bit mask
+#define bit_set(reg, bit_mask) ((reg)|=(bit_mask))//set a bit to 1
+#define bit_clear(reg, bit_mask) ((reg&=~(bit_mask)))//set a bit to 0
+
 #define FULL_PUMP_RATE 10.0/10.85 //unit is mL/s, need to be calibrated for different functions of code
-#define CTR_PIN 10 // Control the transistor to turn on/off the pump
-#define Pump_Time(pump_amount) ((pump_amount*1000.0)/(FULL_PUMP_RATE))
-/*Alternate formula:
- #define SLOW_VOLUME //The percent of the total required volume that the pump will pump at slower rate to get more accuracy
- #define REDUCED_PUMP_RATE 50 //unit is mL/s
- #define Pump_Time(pump_amount,SLOW_VOLUME) ((1.0-SLOW_VOLUME/100.0)*(pump_amount)/(FULL_PUMP_RATE) + (SLOW_VOLUME/100.0)*pump_amount/(REDUCED_PUMP_RATE))
- */
-float test_amount=25.0;//unit is mL
+#define CTR_PIN_PUMP_1 2// Control the transistor to turn on/off the pump
+#define CTR_PIN_PUMP_2 3
+#define CTR_PIN_PUMP_3 4
+
+#define pump1_amount 25.0//unit is mL
+#define pump2_amount 10.0
+#define pump3_amount 15.0
+//change the number below to change the pump amount
+class Pump {
+public:
+float pump_amount;// amount to pump in mL
+unsigned int pump_ID;//2,3, or 4
+
+
+void initiate_pump(unsigned amount, unsigned pin_ID){
+        start_time=millis();
+        pump_amount=amount;
+        pump_ID=pin_ID;
+        pump_time=((pump_amount*1000.0)/(FULL_PUMP_RATE));
+}
+void check_pump(){
+        (millis()-start_time<pump_time) ?  : bit_clear(PORTD,BIT(pump_ID));
+}
+
+private:
+unsigned long start_time;
+unsigned long pump_time;
+};
+
+Pump Pump1;
+Pump Pump2;
+Pump Pump3;
 
 void setup() {
-        TCCR1A = 0x23;//configure the TCCR1A register on mode of operation and TOP
-        TCCR1B = 0x19; //configure the TCCR1B register on mode of operation and TOP
-        OCR1A = 639; // limit by 16 bits, determine the frequency of the output
+        DDRD=0x0E;// set 2,3,4 to output.
+        PORTD=0x00;//turn off everything
+        Serial.begin(115200);
 
-        pinMode(CTR_PIN, OUTPUT); // enable the PWM output (you now have a PWM signal on digital pin 10)
-        //digitalWrite(CTR_PIN, LOW);
-        Serial.begin(9600);
+        Pump1.initiate_pump(pump1_amount, CTR_PIN_PUMP_1);
+        bit_set(PORTD,BIT(CTR_PIN_PUMP_1));
+        Pump2.initiate_pump(pump2_amount, CTR_PIN_PUMP_2);
+        bit_set(PORTD,BIT(CTR_PIN_PUMP_2));
+        Pump3.initiate_pump(pump3_amount, CTR_PIN_PUMP_3);
+        bit_set(PORTD,BIT(CTR_PIN_PUMP_3));
 }
 
 void loop() {
-        static long int start_time=millis();
-        static double pump_duration=Pump_Time(test_amount);
-        if((millis()-start_time)<pump_duration) {
-                OCR1B = 639; // the ratio of OCR1B/OCR1A will set duty cycle
-                Serial.println(pump_duration);
-                //digitalWrite(CTR_PIN, HIGH);
-        }
-        else {
-                OCR1B = 0;//actually stopped the pumping
-                Serial.println("Stopped pumping");
-                delay(1000);
-        }
+        Pump1.check_pump();
+        Pump2.check_pump();
+        Pump3.check_pump();
 }
